973f80f6bcf3254e6ca89c5c11e22677369c27c0
PolinaUshakova2 <48099222+PolinaUshakova2@users.noreply.github.com>
2020-09-30 16:21:43 +0300

    feat: done tasks 1, 2, 6, 9, 10, 12

--- src/main/java/com/rakovets/course/javabasics/practice/operators/Task06.java
+++ src/main/java/com/rakovets/course/javabasics/practice/operators/Task06.java
@@ -12,8 +12,8 @@ public class Task06 {
     public static void main(String[] args) {
         //FIXME
         // Переменные, которые можно изменять для проверки различных вариантов входных параметров
-        int deposit = 1500;
-        int annualPercentage = 3;
+        double deposit = 1500;
+        double annualPercentage = 3;
 
         // Код необходимый для тестирования, не изменять
         deposit = (args.length == 0) ? deposit : Integer.parseInt(args[0]);
@@ -22,5 +22,8 @@ public class Task06 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        double sum = deposit * annualPercentage / 100 * 5;
+        double summa = sum + deposit;
+        System.out.println(summa);
     }
 }
--- src/main/java/com/rakovets/course/javabasics/practice/operators/Task12.java
+++ src/main/java/com/rakovets/course/javabasics/practice/operators/Task12.java
@@ -11,7 +11,7 @@ public class Task12 {
     public static void main(String[] args) {
         //FIXME
         // Переменные, которые можно изменять для проверки различных вариантов входных параметров
-        int playingTimeInSeconds = 156;
+        int playingTimeInSeconds = 1;
 
         // Код необходимый для тестирования, не изменять
         playingTimeInSeconds = (args.length == 1) ? Integer.parseInt(args[0]) : playingTimeInSeconds;
@@ -19,5 +19,12 @@ public class Task12 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        int seconds = playingTimeInSeconds % 60;
+        int min = (playingTimeInSeconds - seconds) / 60;
+        int minutes = min % 60;
+        int h = (min - minutes) / 60;
+        int hours = h % 24;
+        int days = (h - hours) / 24;
+        System.out.println(days + " " + hours + ":" + minutes + ":" + seconds);
     }
 }
